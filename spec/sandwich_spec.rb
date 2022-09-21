8c141fb56e9964768597280136ec1814d9477d80
Edgar Villamarin <edgarv.uribe@hotmail.com>
2022-09-21 15:57:44 -0500

    hooks and poro version

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,17 +1,44 @@
 require 'spec_helper'
 Sandwich = Struct.new(:taste, :toppings)
 
-RSpec.describe 'An ideal sandwich' do
-  before { @sandwich = Sandwich.new('delicious', []) }
 
-  it 'is delicious' do
-    taste = @sandwich.taste
-    expect(taste).to eq('delicious')
+RSpec.context 'hooks version' do
+  def sandwich
+    Sandwich.new('delicious', [])
   end
 
-  it 'lets me add toppings' do
-    @sandwich.toppings << 'cheese'
-    toppings = @sandwich.toppings
-    expect(toppings).not_to be_empty
+  describe 'hooks An ideal sandwich' do
+    before { @sandwich = Sandwich.new('delicious', []) }
+
+    it 'is delicious' do
+      taste = @sandwich.taste
+      expect(taste).to eq('delicious')
+    end
+
+    it 'lets me add toppings' do
+      @sandwich.toppings << 'cheese'
+      toppings = @sandwich.toppings
+      expect(toppings).not_to be_empty
+    end
+  end
+end
+
+RSpec.context 'PORO version' do
+
+  def sandwich
+    @sandwich ||= Sandwich.new('delicious', [])
+  end
+
+  describe 'An ideal sandwich' do
+    it 'is delicious' do
+      taste = sandwich.taste
+      expect(taste).to eq('delicious')
+    end
+
+    it 'lets me add toppings' do
+      sandwich.toppings << 'cheese'
+      toppings = sandwich.toppings
+      expect(toppings).not_to be_empty
+    end
   end
 end
