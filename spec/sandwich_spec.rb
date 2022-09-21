50801184ca62eb9676727557e5d2a1c579e50c73
Edgar Villamarin <edgarv.uribe@hotmail.com>
2022-09-21 13:00:37 -0500

    hooks

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,19 +1,17 @@
 require 'spec_helper'
+Sandwich = Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwich' do
+  before { @sandwich = Sandwich.new('delicious', []) }
 
   it 'is delicious' do
-    Sandwich = Struct.new(:taste, :toppings)
-    sandwich = Sandwich.new('delicious', [])
-    taste = sandwich.taste
+    taste = @sandwich.taste
     expect(taste).to eq('delicious')
   end
 
   it 'lets me add toppings' do
-    sandwich = Sandwich.new('delicious', [])
-
-    sandwich.toppings << 'cheese'
-    toppings = sandwich.toppings
+    @sandwich.toppings << 'cheese'
+    toppings = @sandwich.toppings
     expect(toppings).not_to be_empty
   end
 end
